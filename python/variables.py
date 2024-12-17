f3c1f5f411a319d55a7eb32bf22d9d92a7d2c405
Rockston77 <rockston77@gmail.com>
2024-12-17 15:11:10 +0600

    /python/variables.py i have created this file and its only a excercise

---
+++ python/variables.py
@@ -0,0 +1,33 @@
+# Variables are containers for storing data values.
+# Creating variables
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+x = 5
+y = 'John'
+print(x)
+print(y)
+
+
+## Variables do not need to be declare with any particular type, and can even change type after they have been set.
+x = 4
+x = 'Sally'
+print(x)
+
+## Casting
+# if you want to specify the data type of a variable, this can be done with casting.
+x = str(3)
+y = int(3)
+z = float(3)
+
+## Get the type
+# you can get teh data type of a variable with the type() function.
+a = 4
+b = 'John'
+print(type(a))
+print(type(b))
+
+## Single or double quotes
+# string variables can be declared either by using single or double quotes:
+# Variable names are case-sensitive
+
+
